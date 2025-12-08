7dcd80c2ca73c39f862f028af6f3232b40f70541
Foodword <saahirba07@gmail.com>
2025-12-08 10:25:06 -0800

    Create Item.cpp

---
+++ Item.cpp
@@ -0,0 +1,31 @@
+#include "Item.h"
+
+Item::Item(std::string n, int i, Status s)
+    : name(n), id(i), status(s), due_date(0) {}
+
+std::string Item::getName() { return name; }
+int Item::getID() { return id; }
+Item::Status Item::getStatus() { return status; }
+
+void Item::setDueDate(Calender& cal) {
+    due_date = cal.getDue_Date();
+}
+
+int Item::getDueDate() {
+    return due_date;
+}
+
+void Item::print() {
+    std::cout << "Item ID: " << id << "\n"
+              << "Name: " << name << "\n"
+              << "Status: ";
+
+    switch (status) {
+        case Done:  std::cout << "Done"; break;
+        case ToDo:  std::cout << "To-Do"; break;
+        case Doing: std::cout << "Doing"; break;
+    }
+
+    std::cout << "\nDue Date (MMDDHHMM): " << due_date
+              << "\n-----------------------------\n";
+}
